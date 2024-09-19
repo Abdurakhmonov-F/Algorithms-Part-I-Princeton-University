0b75b13113a09bfaa820ebe8589aa8330f458c43
Abdurakhmonov-F <feruzabdurakhmonov@gmail.com>
2024-09-19 18:16:00 +0500

    Practice Programming Assignment: Hello, World

---
+++ hello/HelloWorld.java
@@ -0,0 +1,11 @@
+package hello;
+/* *****************************************************************************
+ *  Name:              Feruz Abdurakhmonov
+ *  Coursera User ID:  123456
+ *  Last modified:     September 19, 2024
+ **************************************************************************** */
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+    }
+}
---
+++ hello/RandomWord.java
@@ -0,0 +1,29 @@
+package hello;
+
+/* *****************************************************************************
+ *  Name:              Feruz Abdurakhmonov
+ *  Coursera User ID:  123456
+ *  Last modified:     September 19, 2024
+ **************************************************************************** */
+
+import edu.princeton.cs.algs4.StdIn;
+import edu.princeton.cs.algs4.StdOut;
+import edu.princeton.cs.algs4.StdRandom;
+
+public class RandomWord {
+    public static void main(String[] args) {
+        int index = 0;
+        String champion = "";
+
+        while (!StdIn.isEmpty()) {
+            String word = StdIn.readString();
+            boolean accept = StdRandom.bernoulli(1 / (index + 1.0));
+            if (accept) {
+                champion = word;
+            }
+            index++;
+        }
+        StdOut.println(champion);
+    }
+}
+
